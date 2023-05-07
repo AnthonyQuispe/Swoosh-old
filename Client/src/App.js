f3b4269d464ba55055e9a446a0ee8b9a647a101b
“Anthony <Anthony.s.quispe@gmail.com>
2023-05-07 11:09:03 -0400

    made some change to fix Develop branch

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import SignIn from "./Pages/SignIn/SignIn";
+import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/basketball" element={<Dashboard />} />
-      <Route path="/football" element={<Dashboard />} />
-      <Route path="/futbol" element={<Dashboard />} />
+      <Route path="/" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }

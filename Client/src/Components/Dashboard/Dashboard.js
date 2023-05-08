559136a87438dcbebf447ae47612d65994e70551
“Anthony <Anthony.s.quispe@gmail.com>
2023-05-08 00:20:59 -0400

    fixed googleMaps not working

--- Client/src/Components/Dashboard/Dashboard.js
+++ Client/src/Components/Dashboard/Dashboard.js
@@ -1,73 +1,19 @@
-import "./Dashboard.scss";
 import Nav from "../Nav/Nav";
 import BottomNav from "../BottomNavBar/BottomNavBar";
 import Settings from "../Settings/Settings";
+import GoogleMaps from "../GoogleMaps/GoogleMaps";
 import { useState, useEffect } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
-import axios from "axios";
+import profileIcon from "../../assets/Images/Facetune_04-04-2023-11-44-41.jpg";
+import "./Dashboard.scss";
 /* global google */
 
-function Dashboard({ center = { lat: 26.1224, lng: -80.1373 } }) {
+function Dashboard() {
   const [showSettings, setShowSettings] = useState(false);
-
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: "",
-      version: "weekly",
-    });
-
-    loader.load().then(() => {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const { latitude, longitude } = position.coords;
-          const map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: latitude, lng: longitude },
-            zoom: 15,
-            disableDefaultUI: true,
-          });
-        });
-      } else {
-        const map = new google.maps.Map(document.getElementById("map"), {
-          center: { lat: 26.1224, lng: -80.1373 },
-          zoom: 8,
-        });
-      }
-    });
-  }, []);
-
-  // Firebase code
-  // const [apiKey, setApiKey] = useState("");
-
-  // useEffect(() => {
-  //   axios
-  //     .post("https://us-central1-swoosh-1d0a9.cloudfunctions.net/getLatLng")
-  //     .then((response) => {
-  //       setApiKey(response.data.apiKey);
-
-  //       const loader = new Loader({
-  //         apiKey: response.data.apiKey,
-  //         version: "weekly",
-  //       });
-
-  //       loader
-  //         .load()
-  //         .then(() => {
-  //           const map = new google.maps.Map(document.getElementById("map"), {
-  //             center: { lat: 37.7749, lng: -122.4194 },
-  //             zoom: 8,
-  //           });
-  //         })
-  //         .catch((error) => {
-  //           console.error(error);
-  //         });
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }, []);
 
   return (
     <div className="dashboard__container">
+      <GoogleMaps />
       {showSettings && <Settings setShowSettings={setShowSettings} />}
       <Nav setShowSettings={setShowSettings} />
       <div id="map"></div>
--- Client/src/Components/GoogleMaps/GoogleMaps.js
+++ Client/src/Components/GoogleMaps/GoogleMaps.js
@@ -1,11 +1,41 @@
-const getLatLng = firebase.functions().httpsCallable("getLatLng");
+import { Loader } from "@googlemaps/js-api-loader";
+import { useEffect } from "react";
+import profileIcon from "../../assets/Images/Facetune_04-04-2023-11-44-41.jpg";
+/* global google */
 
-getLatLng({ address: "1600 Amphitheatre Parkway, Mountain View, CA" })
-  .then((result) => {
-    const location = result.data;
-    console.log("Latitude:", location.lat);
-    console.log("Longitude:", location.lng);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function Google({ center = { lat: 26.1224, lng: -80.1373 } }) {
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: process.env.REACT_APP_API_KEY,
+      version: "beta",
+    });
+
+    loader.load().then(() => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          const map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: latitude, lng: longitude },
+            zoom: 15,
+            disableDefaultUI: true,
+          });
+
+          const profileMarker = {
+            url: profileIcon,
+            scaledSize: new google.maps.Size(40, 40),
+            className: "profile-marker",
+          };
+
+          // create marker at current location and add it to the map
+          const marker = new google.maps.Marker({
+            position: { lat: latitude, lng: longitude },
+            map: map,
+            icon: profileMarker,
+            animation: google.maps.Animation.DROP,
+          });
+        });
+      }
+    });
+  }, []);
+}
+export default Google;
